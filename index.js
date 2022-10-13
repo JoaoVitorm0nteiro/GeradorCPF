7553959abf6812133753fd177720696e675fdfbb
JoaoVitorm0nteiro <jv147pao@gmail.com>
2022-10-13 15:36:32 -0300

    ajustes codigo

--- index.js
+++ index.js
@@ -1,18 +1,17 @@
 //algoritmo para gerar CPF ->
 const btn = document.querySelector("#buton")
 btn.addEventListener("click" , function (e){
-e.preventDefault()
+e.preventDefault() 
     const gerarCpf = () => {
         let newCpf = '';
         let x = 0;
         let j = 0;
-     
+    
             for(let i = 0; i<9; i++){
               newCpf += (Math.floor(Math.random() * (10 - 0)));
-        }  
-    
+            }    
       //primeiro digito verificador ->
-      for(let i=10; i>=2; i--){
+       for(let i=10; i>=2; i--){
         x += newCpf.charAt(j)*i;
         j++;
       }
@@ -36,10 +35,10 @@ e.preventDefault()
         newCpf += 11 - (x % 11);
       }
     
+      
+
       const result = document.querySelector("#CpfReceiver")
       result.value = innerText = newCpf;    
-      console.log(newCpf);
     } 
     gerarCpf()
 })
-
